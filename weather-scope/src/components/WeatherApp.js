9aba534633cc43c8485ce00b6df245cc2d3db1ff
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-20 23:30:05 +0100

    Update WeatherApp.js

---
+++ weather-scope/src/components/WeatherApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WeatherSelect from "./WeatherSelect";
+import WeatherInputSearch from "./WeatherInputSearch";
+
+const WeatherApp = () => {
+    return (
+        <div>
+        <WeatherSelect />
+        <WeatherInputSearch />
+        </div>
+    );
+    };
+
+export default WeatherApp;
