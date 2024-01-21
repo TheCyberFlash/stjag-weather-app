98bbc6f658e08773ca9bbaf408b341b0254db528
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-21 12:25:07 +0100

    Update WeatherForecast.js

--- weather-scope/src/components/WeatherForecast.js
+++ weather-scope/src/components/WeatherForecast.js
@@ -2,20 +2,26 @@ import React from "react";
 import { useWeatherAPI } from "../context/WeatherAPIContext";
 
 const WeatherForecast = () => {
-    const { weatherDetails } = useWeatherAPI();
-
-    if(!weatherDetails) return null;
+    const { weatherDetails, weatherError } = useWeatherAPI();
+    console.log(weatherError);
+    // if(!weatherDetails) return null;
 
     return (
         <div>
-            <h2>Weather Forecast</h2>
+            
             {weatherDetails && (
                 <div>
+                    <h2>Weather Forecast</h2>
                     <h3>{weatherDetails.name}</h3>
                     <p>Temperature: {weatherDetails.main.temp}</p>
                     <p>Feels Like: {weatherDetails.main.feels_like}</p>
                     <p>Humidity: {weatherDetails.main.humidity}</p>
                     <p>Wind Speed: {weatherDetails.wind.speed}</p>
+                </div>
+            )}
+            {weatherError && (
+                <div>
+                    <h3>{weatherError}</h3>
                 </div>
             )}
         </div>
