f084424265638b44ea1b7840c5519fb39f20b453
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-21 12:14:38 +0100

    Update WeatherApp.js

--- weather-scope/src/components/WeatherApp.js
+++ weather-scope/src/components/WeatherApp.js
@@ -2,6 +2,7 @@ import React from "react";
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import WeatherSelect from "./WeatherSelect";
 import WeatherInputSearch from "./WeatherInputSearch";
+import WeatherForecast from "./WeatherForecast";
 
 const WeatherApp = () => {
     return (
@@ -10,7 +11,7 @@ const WeatherApp = () => {
                 <TiWeatherPartlySunny size={50} color="blue" /> Weather App
             </h1>
             <WeatherSelect />
-            {/* <WeatherInputSearch /> */}
+            <WeatherForecast />
         </div>
     );
 };
