35235b709bf1a92c0c2138696598587d2993c80d
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-21 12:03:05 +0100

    Create WeatherAPIContext.js

---
+++ weather-scope/src/context/WeatherAPIContext.js
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const WeatherAPIContext = createContext();
+
+export const useWeatherAPI = () => {
+    if (useContext(WeatherAPIContext) === undefined) {
+        throw new Error("useWeatherAPI must be used within a WeatherAPIProvider");
+    }
+
+    return useContext(WeatherAPIContext);
+}
+
+const WeatherAPIProvider = ({ children }) => {
+    const [countryOptions, setCountryOptions] = useState([]);
+    const [cityOptions, setCityOptions] = useState([]);
+
+    useEffect(() => {
+        fetchInitalOptions();
+    } ,[]);
+
+    const fetchInitalOptions = () => {
+        fetchCountries();
+        fetchCities("UK");
+    }
+
+    const fetchCountries = () => {
+        fetch("http://api.geonames.org/countryInfoJSON?username=thecyberflash")
+        .then(response => response.json())
+        .then(data => {
+            const countries = data.geonames.map(country => ({ label: country.countryName, value: country.countryCode }));
+            setCountryOptions(countries);
+        })
+    }
+
+    const fetchCities = (country) => {
+        fetch(`http://api.geonames.org/searchJSON?username=thecyberflash&country=${country}`)
+        .then(response => response.json())
+        .then(data => {
+            const cities = data.geonames.map(city => ({ label: city.name, value: city.name }));
+            setCityOptions(cities);
+        })
+    }
+
+    return (
+        <WeatherAPIContext.Provider value={{ countryOptions, cityOptions }}>
+            {children}
+        </WeatherAPIContext.Provider>
+    )
+}
+
+export default WeatherAPIProvider;
