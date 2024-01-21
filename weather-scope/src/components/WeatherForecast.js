d399e472cb7054480829e32c4fa639caab047d7e
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-22 00:39:41 +0100

    Update WeatherForecast.js

--- weather-scope/src/components/WeatherForecast.js
+++ weather-scope/src/components/WeatherForecast.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { useWeatherAPI } from "../context/WeatherAPIContext";
+import { Card, ListGroup } from "react-bootstrap";
 
 const WeatherForecast = () => {
     const { weatherDetails, weatherError } = useWeatherAPI();
 
     return (
-        <div>
+        <div className="mt-4">
             
             {weatherDetails && (
-                <div>
-                    <h2>Weather Forecast</h2>
-                    <h3>{weatherDetails.name}</h3>
-                    <p>Temperature: {weatherDetails.main.temp}</p>
-                    <p>Feels Like: {weatherDetails.main.feels_like}</p>
-                    <p>Humidity: {weatherDetails.main.humidity}</p>
-                    <p>Wind Speed: {weatherDetails.wind.speed}</p>
-                </div>
+                <Card>
+                    <Card.Header as="h2">Weather Forecast</Card.Header>
+                    <Card.Body>
+                        <Card.Title>{weatherDetails.name}</Card.Title>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                Temperature: {weatherDetails.main.temp}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Feels Like: {weatherDetails.main.feels_like}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Humidity: {weatherDetails.main.humidity}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Wind Speed: {weatherDetails.wind.speed}
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>                
             )}
             {weatherError && (
-                <div>
+                <div className="mt-4">
                     <h3>{weatherError}</h3>
                 </div>
             )}
