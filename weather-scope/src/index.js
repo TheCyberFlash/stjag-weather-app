ae8a97d403084519692ffd729a3d9355a550e661
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-22 00:18:47 +0100

    Update index.js

--- weather-scope/src/index.js
+++ weather-scope/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherAPIProvider from './context/WeatherAPIContext';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
