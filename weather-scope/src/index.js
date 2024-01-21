7e2e33ee206bf493dfa401d401dd5ceabc3d8a1c
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-22 00:33:46 +0100

    Update index.js

--- weather-scope/src/index.js
+++ weather-scope/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherAPIProvider from './context/WeatherAPIContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <WeatherAPIProvider>
       <App />
     </WeatherAPIProvider>
-  </React.StrictMode>, document.getElementById('root')
+  </React.StrictMode>
 );
